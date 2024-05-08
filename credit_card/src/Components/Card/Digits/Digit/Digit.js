708c662a2f79be59a9661072301647d7929f2577
TobiAdeniji94 <adenijitobi94@gmail.com>
2024-05-08 16:27:03 +0100

    fixed css

--- credit_card/src/Components/Card/Digits/Digit/Digit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from '../Digit/Digit.module.css';
-
-const Digit = (props) => {
-    return (
-        <div className={classes.Digit} >{props.value}</div>
-    )
-}
-export default Digit;   
